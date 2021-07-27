4b8bc9b6dc83b9296e7e2f59928f067dc0c444ae
Nic <nicollecanuton@gmail.com>
2021-07-27 13:50:54 -0300

    c# stuff

--- csharp/basics/basics/helloworld.cs
+++ csharp/basics/basics/helloworld.cs
@@ -1,13 +1,15 @@
-using System;
+﻿using System;
 
-class basics{ //describes something
-    static void Main(string [] args){
+class basics
+{ //describes something
+    static void Main(string[] args)
+    {
         //static cause it ll be called before class instantiation
         //void cause it does not return data
         //Main sets the entry point to this program
         Console.WriteLine("hello world"); //write line method
         Console.ReadLine(); //just waits to user press something b4 closing the program
-            
+
     }
-    
+
 }
---
+++ csharp/variables/variables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; //language integrated query
+using static System.Console;
+using static System.Convert;
+
+namespace variablesDef
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, secret = 98, chances = 3;
+            Console.WriteLine("SECRET NUMBER GAME");
+            while (a != secret)       
+            {
+                Console.WriteLine("type an int number");
+                a = System.Convert.ToInt32(Console.ReadLine());
+                if(a == secret)
+                {
+                    Console.WriteLine("you did it :)\n");
+                    break;
+                }
+                else
+                {
+                    chances = chances - 1;
+                    Console.WriteLine("thats not the number. you have {0} chances left\n", chances);  
+                    if(chances == 0)
+                    {
+                        Console.WriteLine("you loose. the number was {0}\n", secret);
+                        break;
+
+                    }
+                }
+
+            }
+
+        }
+    }
+}
